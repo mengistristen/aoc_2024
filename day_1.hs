0c058144cf9b3128c1a0e696bd0f9f983496c917
Tristen Mengis <tristen.mengis@gmail.com>
2024-12-01 15:51:33 -0800

    day 1

---
+++ day_1.hs
@@ -0,0 +1,35 @@
+import Data.List
+import qualified Data.Map.Strict as Map
+
+parsePair :: String -> (Int, Int)
+parsePair line = 
+  let [x, y] = map read (words line)
+  in (x, y)
+
+distance :: (Int, Int) -> Int
+distance (x, y) = abs (x - y)
+
+countOccurances :: [Int] -> Map.Map Int Int
+countOccurances input = 
+  foldl (\acc num -> Map.insertWith (+) num 1 acc) Map.empty input
+
+calculateSimilarity :: Int -> Map.Map Int Int -> Int
+calculateSimilarity value table = case Map.lookup value table of 
+  Just y -> value * y
+  Nothing -> 0
+
+partOne :: String -> IO()
+partOne inputFile = do
+  content <- readFile inputFile
+  let (x, y) = unzip (map parsePair (lines content))
+  let sorted = zip (sort x) (sort y)
+  let result = sum (map distance sorted)
+  print result
+
+partTwo :: String -> IO()
+partTwo inputFile = do
+  content <- readFile inputFile
+  let (x, y) = unzip (map parsePair (lines content))
+  let occurances = countOccurances y
+  let result = sum (map (\value -> calculateSimilarity value occurances) x)
+  print result
