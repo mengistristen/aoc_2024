56fb23ddd65c621aca2e949fe46b17fdc8feb321
Tristen Mengis <tristen.mengis@gmail.com>
2024-12-03 13:45:05 -0800

    day 2, part 2

--- day_2.hs
+++ day_2.hs
@@ -1,31 +1,34 @@
-process :: [Int] -> Bool
-process (x:y:rest)
-  | x == y = False
-  | x - y > 0 = decreasing (x:y:rest)
-  | otherwise = increasing (x:y:rest)
-process [x] = True
+process :: [Int] -> Int -> Bool
+process (x : y : rest) tolerance
+  | x == y = (tolerance > 0) && process (y : rest) (tolerance - 1)
+  | otherwise = check [] (x : y : rest) (<) tolerance || check [] (x : y : rest) (>) tolerance
+process [x] _ = True
 
-decreasing :: [Int] -> Bool
-decreasing (x:y:rest)
-  | x == y = False
-  | y > x = False
-  | x - y > 3 = False
-  | otherwise = decreasing (y:rest)
-decreasing [x] = True
+check :: [Int] -> [Int] -> (Int -> Int -> Bool) -> Int -> Bool
+check accepted (x : y : rest) compare tolerance
+  | x == y = (tolerance > 0) && check [] (accepted ++ y : rest) compare (tolerance - 1)
+  | compare x y || abs (x - y) > 3 =
+      (tolerance > 0)
+        && ( check [] (accepted ++ x : rest) compare (tolerance - 1)
+               || check [] (accepted ++ y : rest) compare (tolerance - 1)
+           )
+  | otherwise = check (accepted ++ [x]) (y : rest) compare tolerance
+check _ [x] _ _ = True
 
-increasing :: [Int] -> Bool
-increasing (x:y:rest)
-  | x == y = False
-  | y < x = False
-  | y - x > 3 = False
-  | otherwise = increasing (y:rest)
-increasing [x] = True
-
-partOne :: String -> IO()
+partOne :: String -> IO ()
 partOne input = do
   content <- readFile input
   let split = lines content
   let text = map words split
-  let reports :: [[Int]]= map (map read) text
-  let result = map process reports
+  let reports :: [[Int]] = map (map read) text
+  let result = map (`process` 0) reports
+  print (length (filter id result))
+
+partTwo :: String -> IO ()
+partTwo input = do
+  content <- readFile input
+  let split = lines content
+  let text = map words split
+  let reports :: [[Int]] = map (map read) text
+  let result = map (`process` 1) reports
   print (length (filter id result))
