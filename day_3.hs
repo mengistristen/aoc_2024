61deaf9c44bb6dfe85de6ec64a595777b51fb4ff
Tristen Mengis <tristen.mengis@gmail.com>
2024-12-04 01:06:01 -0800

    run formatter

--- day_3.hs
+++ day_3.hs
@@ -3,21 +3,21 @@ module Main where
 import Parser
 
 newtype Mul = Mul (Int, Int)
-  deriving (Show) 
+  deriving (Show)
 
 mulP :: Parser Mul
 mulP = stringP "mul" *> charP '(' *> pair <* charP ')'
-  where 
-    pair = 
+  where
+    pair =
       (\left _ right -> Mul (left, right)) <$> numP <*> charP ',' <*> numP
 
 process :: String -> Int -> Int
 process input accum = case runParser mulP input of
   Just ([], Mul (x, y)) -> accum + (x * y)
-  Just (rest, Mul(x, y)) -> process rest (accum + (x * y))
-  Nothing -> case input of 
+  Just (rest, Mul (x, y)) -> process rest (accum + (x * y))
+  Nothing -> case input of
     [] -> accum
-    (_:rest) -> process rest accum
+    (_ : rest) -> process rest accum
 
 partOne :: String -> IO ()
 partOne input = do
