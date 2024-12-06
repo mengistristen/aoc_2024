d5c0d6c9749f7f7503af089e4adf9af2a216ca59
Tristen Mengis <tristen.mengis@gmail.com>
2024-12-06 11:03:10 -0800

    day 4, part 1

--- day_4.hs
+++ day_4.hs
@@ -1,14 +1,15 @@
 module Main where
 
+import Debug.Trace
 import Data.List
 
 directions = [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
 
 getIndices :: Int -> Int -> [(Int, Int)]
-getIndices rows cols = [(x, y) | x <- [0..rows - 1], y <- [0..cols - 1]]
+getIndices rows cols = [(x, y) | x <- [0 .. rows - 1], y <- [0 .. cols - 1]]
 
 getElement :: [[Char]] -> (Int, Int) -> Maybe Char
-getElement input (x, y) 
+getElement input (x, y)
   | x < 0 || x >= length (head input) = Nothing
   | y < 0 || y >= length input = Nothing
   | otherwise = Just ((input !! y) !! x)
@@ -19,22 +20,26 @@ process input =
 
 processCell :: [[Char]] -> (Int, Int) -> Int
 processCell input (x, y)
-  | input !! y !! x == 'X' = sum $ map (processWord input (x, y) "X") directions
+  | input !! y !! x == 'X' = sum $ map (processWord input (x, y) "") directions
   | otherwise = 0
 
 processWord :: [[Char]] -> (Int, Int) -> [Char] -> (Int, Int) -> Int
-processWord input (x, y) accum (dx, dy)  
-  = case getElement input (x, y) of 
+processWord input (x, y) accum (dx, dy) =
+  case getElement input (x, y) of
     Nothing -> 0
-    Just (value) -> 
-      let str = accum ++ [char]
-      in if str == "XMAS"
-        then 1
-        else if str `isPrefixOf` "XMAS"
-          then processWord input (x + dx, y + dy)
-  -- | str == "XMAS" = 1  
-  -- | str `isPrefixOf` "XMAS" = processWord input (x + dx, y + dy) str (dx, dy)
-  -- | otherwise = 0
+    Just value ->
+      let str = accum ++ [value]
+       in if str == "XMAS"
+            then 1
+            else
+              if str `isPrefixOf` "XMAS"
+                then processWord input (x + dx, y + dy) str (dx, dy)
+                else 0
+
+partOne :: String -> IO()
+partOne inputFile = do
+  content <- readFile inputFile
+  print $ sum $ process $ lines content
 
 main :: IO ()
 main = do
